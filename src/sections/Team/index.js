065de033fada3b3071b9159c7266e006cf5bf344
Nelson Estevão <nelsonmestevao@gmail.com>
2019-08-16 16:15:20 +0100

    Update list of members (#9)

--- src/sections/Team/index.js
+++ src/sections/Team/index.js
@@ -2,34 +2,30 @@ import React from "react";
 import Heading from "../../components/Heading";
 import Text from "../../components/Text";
 
+const members = [
+  "Ângela Morais",
+  "Catarina Lopes",
+  "Fernando Mendes",
+  "Francisco Sousa",
+  "Hugo Gonçalves",
+  "Regina Sousa",
+  "Sofia Sepúlveda",
+];
+
+const createMemberElement = member => <li>{member}</li>;
+
 export default function Team() {
   return (
     <section id="team">
       <Heading level="3">$ users</Heading>
       <Text>
-        Somos 10 programadores, 1 designer, 3 estudantes de comunicação, 1
-        estudante de biomédica e 1 estudante de Línguas e Literaturas.
+        Somos 4 programadores, 2 estudantes de biomédica e 1 de biológica.
         Procuramos diversidade e formas diferentes de pensar. Procuramos
         irreverência. Acreditamos que todos tem um papel relevante e igual
         dentro da equipa. Não somos diferentes, apenas temos perspetivas
         diferentes. Os nossos nomes, anarquicamente escritos:
         <ul>
-          <li>Ana Patrícia Magalhães</li>
-          <li>Andreia Miranda</li>
-          <li>Ângela Morais</li>
-          <li>Bruno Cancelinha</li>
-          <li>Catarina Lopes</li>
-          <li>Fernando Mendes</li>
-          <li>Francisco Baila</li>
-          <li>Francisco Sousa</li>
-          <li>Inês Sampaio</li>
-          <li>Joana Ferreira</li>
-          <li>Liliana Pinheiro Gonçalves</li>
-          <li>Luís Torres</li>
-          <li>Miguel Noivo</li>
-          <li>Nelson Estevão</li>
-          <li>Ricardo Pereira</li>
-          <li>Zé Azevedo</li>
+          {members.sort(Intl.Collator().compare).map(createMemberElement)}
         </ul>
       </Text>
     </section>
